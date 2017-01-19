648f345f4bcf2e42fae3cb5713e91b13726c9193
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2017-01-19 21:50:34 +0000

    Added encoders and decoders for void, bool, int, Length (with automatic conversion to Revit's feet), LengthArray, ElementId, Element, Level. Primitive operations were fixed to use these types. Several new primitive operations added.

--- revit-server/primitives.rkt
+++ revit-server/primitives.rkt
@@ -8,10 +8,20 @@
   (set! id-counter (+ id-counter 1))
   id-counter)
 
-(defdecoder void identity)
+(defdecoder void
+  (lambda ([i : Input-Port])
+    (let ((code (read-byte i)))
+      (if (= code 0)
+          #t
+          (error 'Revit "Error: ~A" (read-c-sharp-string i))))))
 
 (defencoder bool (lambda ([b : Boolean] [o : Output-Port]) (write-byte (if b 1 0) o)))
-(defdecoder bool (lambda ([i : Input-Port]) (= (read-byte i) 1)))
+(defdecoder bool
+  (lambda ([i : Input-Port])
+    (let ((val (read-byte i)))
+      (if (= val 127)
+          (error 'Revit "Error: ~A" (read-c-sharp-string i))
+          (= val 1)))))
 
 (defencoder string write-c-sharp-string)
 (defdecoder string read-c-sharp-string)
@@ -19,46 +29,55 @@
 (defencoder double write-double)
 (defdecoder double read-double)
 
-(defencoder Int32 write-int32)
-(defdecoder Int32 read-int32)
+(defencoder int write-int32)
+(defdecoder int read-int32)
 
 (defencoder byte write-byte)
 (defdecoder byte read-byte)
 
+(define to-feet 3.28084)
+(define (write-length l o)
+  (write-double (* l to-feet) o))
+(define (read-length i)
+  (/ (read-double i) to-feet))
+
+(defencoder Length write-length)
+(defdecoder Length read-length)
+
 (defencoder XYZ
   (lambda ([p : Loc] [o : Output-Port])
     (let ((p (loc-in-world p)))
-      (write-double (cx p) o)
-      (write-double (cy p) o)
-      (write-double (cz p) o))))
+      (write-length (cx p) o)
+      (write-length (cy p) o)
+      (write-length (cz p) o))))
 
 (defdecoder XYZ
   (lambda ([i : Input-Port])
-    (xyz (read-double i)
-         (read-double i)
-         (read-double i))))
+    (xyz (read-length i)
+         (read-length i)
+         (read-length i))))
 
 (defencoder Vector3d
   (lambda ([v : Vec] [o : Output-Port])
     (let ((v (vec-in-world v)))
-      (write-double (cx v) o)
-      (write-double (cy v) o)
-      (write-double (cz v) o))))
+      (write-length (cx v) o)
+      (write-length (cy v) o)
+      (write-length (cz v) o))))
 
 (defdecoder Vector3d
   (lambda ([i : Input-Port])
-    (vxyz (read-double i)
-          (read-double i)
-          (read-double i))))
+    (vxyz (read-length i)
+          (read-length i)
+          (read-length i))))
 
 (defencoder XYZArray
   (lambda ([pts : Locs] [o : Output-Port])
     (write-int32 (length pts) o)
     (for ([pt : Loc (in-list pts)])
       (let ((p (loc-in-world pt)))
-        (write-double (cx p) o)
-        (write-double (cy p) o)
-        (write-double (cz p) o)))))
+        (write-length (cx p) o)
+        (write-length (cy p) o)
+        (write-length (cz p) o)))))
 
 (defencoder stringArray
   (lambda ([strs : (Listof String)] [o : Output-Port])
@@ -72,10 +91,37 @@
     (for ([d : Real (in-list ds)])
       (write-double d o))))
 
-(defencoder ElementId
-  write-int32)
+(defencoder LengthArray
+  (lambda ([ls : (Listof Real)] [o : Output-Port])
+    (write-int32 (length ls) o)
+    (for ([l : Real (in-list ls)])
+      (write-length l o))))
 
-(defdecoder ElementId
+
+(define (verified-read-id [i : Input-Port])
+  (let ((id (read-int32 i)))
+      (if (= id -1)
+          (error 'Revit "Error: ~A" (read-c-sharp-string i))
+          id)))
+
+(defencoder ElementId write-int32)
+(defdecoder ElementId verified-read-id)
+
+(defdecoder ElementIdArray 
+  (lambda ([i : Input-Port])
+    (let ((size (read-int32 i)))
+      (if (= size -1)
+          (error 'Revit "Error: ~A" (read-c-sharp-string i))
+          (for/list ([n : Integer (in-range size)])
+            (read-int32 i))))))
+
+(defencoder Element write-int32)
+(defdecoder Element verified-read-id)
+
+(defencoder Level write-int32)
+(defdecoder Level verified-read-id)
+
+(defdecoder Level
   (lambda ([i : Input-Port])
     (let ((id (read-int32 i)))
       (if (= id -1)
@@ -87,11 +133,31 @@
 ;Revit
 (use-port-number! 11001)
 
-(defop "public ElementId FindOrCreateLevelAtElevation(double elevation)")
-(defop "public ElementId UpperLevel(ElementId currentLevelId, double addedElevation)")
+;;Note that doubles that represent lengths need to use the Length type so that we convert to
+;;Revit's internal units (i.e., feet)
+(defop "public ElementId FindOrCreateLevelAtElevation(Length elevation)")
+(defop "public ElementId UpperLevel(ElementId currentLevelId, Length addedElevation)")
+(defop "public Length GetLevelElevation(Level level)")
 (defop "public ElementId LoadFamily(string fileName)")
-(defop "public ElementId FamilyElement(ElementId familyId, string[] namesList, double[] valuesList)")
+(defop "public ElementId FamilyElement(ElementId familyId, string[] namesList, Length[] valuesList)")
 (defop "public ElementId CreatePolygonalFloor(XYZ[] pts, ElementId levelId)")
 (defop "public ElementId CreatePolygonalRoof(XYZ[] pts, ElementId levelId, ElementId famId)")
-(defop "public ElementId CreateColumn(XYZ location, ElementId baseLevelId, ElementId topLevelId, ElementId famId)")
+(defop "public Element CreateColumn(XYZ location, ElementId baseLevelId, ElementId topLevelId, ElementId famId)")
+(defop "public Element CreateColumnPoints(XYZ p0, XYZ p1, Level level0, Level level1, ElementId famId)")
+(defop "public ElementId[] CreateLineWall(XYZ[] pts, ElementId baseLevelId, ElementId topLevelId, ElementId famId)")
+(defop "public ElementId CreateSplineWall(XYZ[] pts, ElementId baseLevelId, ElementId topLevelId, ElementId famId, bool closed)")
+(defop "public Element CreateLineRailing(XYZ[] pts, ElementId baseLevelId, ElementId familyId)") 
+(defop "public Element CreatePolygonRailing(XYZ[] pts, ElementId baseLevelId, ElementId familyId)")
 (defop "public ElementId CreateBeam(XYZ p0, XYZ p1, ElementId famId)")
+(defop "public Element SurfaceGrid(XYZ[] linearizedMatrix, int n, int m)")
+(defop "public void MoveElement(ElementId id, XYZ translation)")
+(defop "public void RotateElement(ElementId id, double angle, XYZ axis0, XYZ axis1)")
+(defop "public void CreatePolygonalHole(XYZ[] pts, Element host)")
+(defop "public Element InsertDoor(Length deltaFromStart, Length deltaFromGround, Element host, ElementId familyId)")
+(defop "public Element InsertWindow(Length deltaFromStart, Length deltaFromGround, Element host, ElementId familyId)")
+(defop "public Element InsertRailing(Element host, ElementId familyId)")
+(defop "public void HighlightElement(ElementId id)")
+(defop "public ElementId[] GetSelectedElements()")
+(defop "public bool IsProject()")
+(defop "public void DeleteAllElements()")
+(defop "public void SetView(XYZ camera, XYZ target, double focal_length)")
