1882b6d43687f8fdce03eddd00f176370f0af215
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2018-10-17 10:24:51 +0100

    Missing parameter.

--- base/shapes.rkt
+++ base/shapes.rkt
@@ -558,7 +558,7 @@
 (def-base-shape 3D-shape (window [wall : Any] [loc : Loc] [family : Any]))
 (def-base-shape 3D-shape (panel [vertices : Locs] [level : Any] [family : Any]))
 (def-base-shape 3D-shape (truss-node [p : Loc] [family : Any] [reuse? : Bool #f]))
-(def-base-shape 3D-shape (truss-bar [p0 : Loc] [p1 : Loc] [angle : Real] [family : Any]))
+(def-base-shape 3D-shape (truss-bar [p0 : Loc] [p1 : Loc] [angle : Real] [family : Any] [reuse? : Bool #f]))
 
 ;;Properties
 
